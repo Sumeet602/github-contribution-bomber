8adf20b8d7f0f648675b818e44933bc4484c8cb0
dhruv-mavani <mavanidhruv3@gmail.com>
2026-07-08 17:02:36 +0530

    enable hardware antialiasing for maximum sharpness

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -55,12 +55,11 @@ export const Scene: React.FC<MyCompositionProps> = ({ levels, targets, flightPat
     <ThreeCanvas 
       width={width} 
       height={height} 
+      gl={{ antialias: true }}
     >
       <color attach="background" args={["#030508"]} />
       <React.Suspense fallback={null}>
         <POVCamera flightPath={flightPath} durationInFrames={durationInFrames} targets={targets} />
-        <Stars />
-        <SolarSystem />
         <ambientLight intensity={1.5} />
         <directionalLight position={[10, 20, 10]} intensity={2.5} castShadow />
         
